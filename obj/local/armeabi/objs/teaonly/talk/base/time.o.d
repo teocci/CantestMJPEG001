15a549982c265d7616cb64242fbb291142ae812d
Teocci <jrfdeft@gmail.com>
2016-06-10 19:08:26 +0900

    first commit

---
+++ obj/local/armeabi/objs/teaonly/talk/base/time.o.d
@@ -0,0 +1,11 @@
+obj/local/armeabi/objs/teaonly/talk/base/time.o: jni/talk/base/time.cc \
+ jni/talk/base/common.h jni/talk/base/basictypes.h \
+ jni/talk/base/constructormagic.h jni/talk/base/time.h
+
+jni/talk/base/common.h:
+
+jni/talk/base/basictypes.h:
+
+jni/talk/base/constructormagic.h:
+
+jni/talk/base/time.h:
